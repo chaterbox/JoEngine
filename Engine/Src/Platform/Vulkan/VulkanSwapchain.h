60a563311021302a1b676dec2957c265fa374456
chaterbox <chatbox8558@gmail.com>
2024-06-13 20:36:28 -0500

    slim down swapchain

--- Engine/Src/Platform/Vulkan/VulkanSwapchain.h
+++ Engine/Src/Platform/Vulkan/VulkanSwapchain.h
@@ -1,31 +1,17 @@
 #pragma once
 #include <vulkan.h>
-#include <vector>
 
 struct GLFWwindow;
 
 namespace Joe{
 	class VulkanSwapchain{
 	public:
-		void Create(VkInstance instance,VkPhysicalDevice phyDevice,VkDevice logDevice,bool VSync);
-		static void CreateSurface(VkInstance instance, GLFWwindow* m_WindowHandle);
-		void CreateImageViews(VkDevice logDevice);
+		void Create(bool VSync);
 
-		VkSurfaceKHR GetSurfaceKHRHandle() { return surface; }
-
-		const std::vector<VkImageView> GetImageViewHandle() { return m_Imageview; }
-		const std::vector<VkImage> GetSwapImagesHandle() { return m_SwapImages; }
 		VkSwapchainKHR GetSwapchainKHRHandle() { return m_Swapchain; }
 	private:
-		static inline VkSurfaceKHR surface;
-
 		VkSwapchainKHR m_Swapchain;
 		VkSurfaceCapabilitiesKHR surCap;
 		VkSurfaceFormatKHR surFormat;
-		
-		uint32_t swapchainCount{ 0u };
-		std::vector<VkImage> m_SwapImages;
-
-		std::vector<VkImageView> m_Imageview;
 	};
 }
