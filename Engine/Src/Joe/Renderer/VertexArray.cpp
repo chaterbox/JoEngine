be709c4c3904f819f1b122e87b6cdd175ec8ec2e
chaterbox <chatbox8558@gmail.com>
2024-06-13 20:39:07 -0500

    Update VertexArray.cpp

--- Engine/Src/Joe/Renderer/VertexArray.cpp
+++ Engine/Src/Joe/Renderer/VertexArray.cpp
@@ -4,14 +4,13 @@
 #include "Renderer.h"
 
 #include "Platform/Opengl/OpenGLVertexArray.h"
-#include "Platform/Vulkan/VulkanVertexArray.h"
 
 namespace Joe{
 	Joe::VertexArray* VertexArray::Create(){
 		switch (Renderer::GetAPI()){
 		case RendererAPI::API::None:   JOE_CORE_ASSERT(false, "RendererAPI::None is currently not supported"); return nullptr;
 		case RendererAPI::API::OpenGL: return new OpenGLVertexArray();
-		case RendererAPI::API::Vulkan: return new VulkanVertexArray();
+		case RendererAPI::API::Vulkan: return nullptr;
 		case RendererAPI::API::DX12: return nullptr;
 		}
 		JOE_CORE_ASSERT(false, "RendererAPI is unknown");
