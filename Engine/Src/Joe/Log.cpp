3072427d74a2e55f4c3eddc602dd0ccd7f7bee85
chaterbox <chatbox8558@gmail.com>
2024-06-15 21:05:13 -0500

    Update Log.cpp

--- Engine/Src/Joe/Log.cpp
+++ Engine/Src/Joe/Log.cpp
@@ -1,17 +1,20 @@
 #include "Joepch.h"
+#include "spdlog/common.h"
+#include "spdlog/spdlog.h"
 #include "Log.h"
+#include <memory>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace Joe{
-	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
-	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger = std::make_shared<spdlog::logger>("JOVK");
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger = std::make_shared<spdlog::logger>("APP");
 
 	void Log::Init(){
 		spdlog::set_pattern("%^ [%T] %n: %v%$");
-		s_CoreLogger = spdlog::stdout_color_mt("JOVK");
+    s_CoreLogger->sinks().push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
 		s_CoreLogger->set_level(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger->sinks().push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
 		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 }
