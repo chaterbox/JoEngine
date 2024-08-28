4eb15ec18e15a52526171ab2d57a19f82df04954
chaterbox <chatbox8558@gmail.com>
2024-08-28 03:12:53 -0500

    Update compile-compress-dist.sh

--- Scripts/Linux/compile-compress-dist.sh
+++ Scripts/Linux/compile-compress-dist.sh
@@ -5,3 +5,9 @@ echo
 echo compressing build 
 echo
 tar -czvf JoEngine_Linux.tar.gz bin/Sandbox/Sandbox bin/Engine/libEngine.a Assets
+echo
+echo compression of JoEngine_Linux DONE
+rm bin/Sandbox/Sandbox
+rmdir bin/Sandbox
+rm bin/Engine/libEngine.a
+rmdir bin/Engine
