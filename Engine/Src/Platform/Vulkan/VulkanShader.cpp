3a4ef36e06edac5d52fb180a9d59232ed422964e
chaterbox <chatbox8558@gmail.com>
2024-08-27 20:43:56 -0500

    Update VulkanShader.cpp
    add a SPIR-V and folder check

--- Engine/Src/Platform/Vulkan/VulkanShader.cpp
+++ Engine/Src/Platform/Vulkan/VulkanShader.cpp
@@ -4,6 +4,7 @@
 #include <vulkan/vulkan.h>
 #include <shaderc/shaderc.hpp>
 #include <cstdint>
+#include <filesystem>
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -13,18 +14,28 @@ namespace Joe{
                 ///////////////////////////////////////////
                 //////   COMPILE SHADER TO SPIR-V    //////
                 ///////////////////////////////////////////
+    bool dev = false;
+    if(!std::filesystem::exists("../../../Assets/Shaders/SPIR-V")){
+      std::filesystem::create_directory("../../../Assets/Shaders/SPIR-V");
+    }
+    if(!std::filesystem::exists("../../../Assets/Shaders/SPIR-V/" + ShaderName + ".spv")){
+      JOE_CORE_ERROR("VULKAN::SHADER::SPIR-V::NONE-EXIST");
 
     //TODO: copy shader file to string  
-    std::ifstream ShaderFile(ShaderPath);
+      std::ifstream ShaderFile(ShaderPath);
+    
+      if(ShaderFile.is_open()){
 
-    if(ShaderFile.is_open()){
-
+      }
+      JOE_CORE_INFO("VULKAN::SHADER::SPIR-V::{0}::COMPILING",ShaderName);
     }
-
+    else{
+      JOE_CORE_INFO("SPIR-V::{0}::FOUND",ShaderName);
+    }
                 ///////////////////////////////////////////
                 //////////   READ SPIR-V file    //////////
                 ///////////////////////////////////////////
-
+  
     //TODO: compile shaders into spirv at runtime
     #ifdef JOE_DIST
       std::ifstream shaderSpirVFile("../../Assets/Shaders/SPIR-V/" + ShaderName + ".spv", std::ios::ate | std::ios::binary);
@@ -33,15 +44,9 @@ namespace Joe{
     #endif
     //check if spirv file found and log type of shader found or not found 
     if(!shaderSpirVFile.is_open()){
-      if(shaderType == ShaderType::Vert)
-        JOE_CORE_FATAL("VULKAN::SHADER::VERTEX::SPIR-V::LOAD::FAILED");
-      if(shaderType == ShaderType::Frag)
-        JOE_CORE_FATAL("VULKAN::SHADER::FRAGMENT::SPIR-V::LOAD::FAILED") ;
+        JOE_CORE_FATAL("VULKAN::SHADER::{0}::SPIR-V::LOAD::FAILED",ShaderName);
     }else{
-      if(shaderType == ShaderType::Vert)
-        JOE_CORE_INFO("VULKAN::SHADER::VERTEX::SPIR-V::LOAD::SUCCESS");
-      if(shaderType == ShaderType::Frag)
-        JOE_CORE_INFO("VULKAN::SHADER::FRAGMENT::SPIR-V::LOAD::SUCCESS");
+        JOE_CORE_INFO("VULKAN::SHADER::{0}::SPIR-V::LOAD::SUCCESS",ShaderName);
     }
 
     //get filesize
@@ -68,17 +73,13 @@ namespace Joe{
     
     //create shader module
     if(vkCreateShaderModule(VulkanContext::GetLogicalDeviceHandle(), &createInfo, nullptr, &shader) != VK_SUCCESS){
-      if(shaderType == ShaderType::Vert)
-        JOE_CORE_FATAL("VULKAN::SHADER::VERTEX::MODULE::CREATION::FAILED");
-      if(shaderType == ShaderType::Frag)
-        JOE_CORE_FATAL("VULKAN::SHADER::FRAGMENT::MODULE::CREATION::FAILED");
+        JOE_CORE_FATAL("VULKAN::SHADER::{0}::MODULE::CREATION::FAILED",ShaderName);
     }else{
-      if(shaderType == ShaderType::Vert)
-        JOE_CORE_INFO("VULKAN::SHADER::VERTEX::MODULE::CREATION::SUCCESS");
-      if(shaderType == ShaderType::Frag)
-        JOE_CORE_INFO("VULKAN::SHADER::FRAGMENT::MODULE::CREATION::SUCCESS");
+        JOE_CORE_INFO("VULKAN::SHADER::{0}::MODULE::CREATION::SUCCESS",ShaderName);
     }
-      
+
+    std::cout << "\n";    
+
     return shader;
   }
 }
