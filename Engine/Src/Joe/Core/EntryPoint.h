de3146963ef7b5a5220e2907e3ec1bf48cc4604d
chaterbox <chatbox8558@gmail.com>
2024-09-17 20:19:02 -0500

    Update EntryPoint.h
    log display protocol and desktop environment

--- Engine/Src/Joe/Core/EntryPoint.h
+++ Engine/Src/Joe/Core/EntryPoint.h
@@ -19,12 +19,17 @@ int main(){
     struct utsname details;
     int ret = uname(&details);
 
+    std::string wProtocolName = std::getenv("XDG_SESSION_TYPE");
+    std::string deName = std::getenv("XDG_SESSION_DESKTOP");
+
     if(ret == 0){
       JOE_CORE_INFO("OS::{0}",details.sysname);
       JOE_CORE_INFO("OS::DISTRO::{0}",details.nodename);
       JOE_CORE_INFO("OS::DISTRO::VERSION::{0}",details.version);
       JOE_CORE_INFO("OS::KERNEL::VERSION::{0}",details.release);
-      JOE_CORE_INFO("OS::ARCHITECTURE::{0}",details.machine);
+      JOE_CORE_INFO("OS::ARCHITECTURE::{0}\n",details.machine);
+      JOE_CORE_INFO("OS::DISPLAY::PROTOCOL::{0}",wProtocolName);
+      JOE_CORE_INFO("OS::DESKTOP_ENVIROMENT::{0}",deName);
     }
   #endif // LINUX
   auto app = Joe::CreateApplication();
