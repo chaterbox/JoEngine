a4f262049dc89711e2f23f37996734819a278316
chaterbox <chatbox8558@gmail.com>
2024-09-20 18:35:55 -0500

    Update Log.cpp
    enable output log to text files

--- Engine/Src/Joe/Core/Log.cpp
+++ Engine/Src/Joe/Core/Log.cpp
@@ -4,17 +4,31 @@
 #include "Log.h"
 #include <memory>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Joe{
-	std::shared_ptr<spdlog::logger> Log::s_CoreLogger = std::make_shared<spdlog::logger>("JOVK");
-	std::shared_ptr<spdlog::logger> Log::s_ClientLogger = std::make_shared<spdlog::logger>("APP");
+  std::shared_ptr<spdlog::logger> Log::s_CoreLogger = std::make_shared<spdlog::logger>("JOVK");
+  std::shared_ptr<spdlog::logger> Log::s_ClientLogger = std::make_shared<spdlog::logger>("APP");
 
-	void Log::Init(){
-		spdlog::set_pattern("%^ [%T] %n: %v%$");
+  void Log::Init(){
+    std::string engineLogPath,appLogPath;
+
+    #ifndef JOE_DIST
+      engineLogPath = "../../../Assets/Logs/Engine.txt";
+      appLogPath = "../../../Assets/Logs/App.txt";
+    #endif
+    #ifdef JOE_DIST
+      engineLogPath = "../../Assets/Logs/Engine.txt";
+      appLogPath = "../../Assets/Logs/App.txt";
+    #endif
+
+    spdlog::set_pattern("%^ [%T] %n: %v%$");
     s_CoreLogger->sinks().push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		s_CoreLogger->set_level(spdlog::level::trace);
-
-		s_ClientLogger->sinks().push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		s_ClientLogger->set_level(spdlog::level::trace);
-	}
+    s_CoreLogger->sinks().push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(engineLogPath));
+    s_CoreLogger->set_level(spdlog::level::trace);
+   
+    s_ClientLogger->sinks().push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+    s_ClientLogger->sinks().push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(appLogPath));
+    s_ClientLogger->set_level(spdlog::level::trace);
+  }
 }
